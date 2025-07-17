6a2fef95d9abc2a51ca43fb5588797ce5b44b976
Isaac David Quintero Lobo <isaacquintero4k@gmail.com>
2025-07-16 19:54:37 -0500

    feat: card creation and general views and functional inputs

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,21 +1,74 @@
 import "../css/style.css";
-import { CardComponent } from "../layout/card_ia/card";
 
-// Obtén el contenedor principal
-const app = document.getElementById("app");
+type Task = {
+  id: string;
+  title: string;
+  description: string;
+  state: "todo" | "in_progrees" | "done";
+};
 
-if (app) {
-  // Crea una card y agrégala al contenedor
-  const cardsData = [
-    {title:"card 1", content:"contenido dimanico"},
-  ];
+type TaskStructure = {
+  todo: Task[];
+  in_progrees: Task[];
+  done: Task[];
+};
+
+let Tasks: TaskStructure = {
+  todo: [],
+  in_progrees: [],
+  done: [],
+};
 
 
-      
-  cardsData.forEach((data)=>{
-    const card = new CardComponent(data.title, data.content);
-    card.render(app);
-  })
- 
-
+function generateId():string{
+    return Math.random().toString(36).substring(2,10)
 }
+
+function createCard(task: Task): HTMLElement{
+    const card = document.createElement('div')
+    card.className = "bg-neutral-700 p-3 rounded text-white shadow-md"
+    card.textContent = task.title;
+    card.setAttribute("data-id", task.id);
+    return card
+}
+
+function renderInitialTasks(){
+    Tasks.todo.forEach((task)=>{
+        const column = document.getElementById("todo-list")
+        if (column) {
+            const card = createCard(task)
+            column.appendChild(card)
+        }
+    })
+    
+};
+
+function setupInput(inputId: string, listId: string){
+    const input = document.getElementById(inputId) as HTMLInputElement;
+    const list = document.getElementById(listId)!;
+
+    input.addEventListener("keydown", (e)=>{
+        if (e.key === "Enter" && input.value.trim() !== "") {
+            const newTask: Task = {
+                id: generateId(),
+                title: input.value.trim(),
+                description: "",
+                state: "todo"
+            };
+
+            Tasks.todo.push(newTask);
+
+            const card = createCard(newTask);
+            list.appendChild(card)
+
+            input.remove();
+            
+        }
+    })
+}
+
+renderInitialTasks()
+
+setupInput("todo-input", "todo-list");
+setupInput("progress-input", "progress-list");
+setupInput("done-input","done-list")
